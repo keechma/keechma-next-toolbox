f9c29f1b7c3ecf81ad603085f9c9a694744dd89e
Cukac <ivan.curic.grad@gmail.com>
2020-08-12 18:19:30 +0200

    add logging

---
+++ src/keechma/next/toolbox/logging.cljs
@@ -0,0 +1,35 @@
+(ns keechma.next.toolbox.logging
+  (:require [oops.core :refer [oget ocall oapply+ oget+ ocall+]]))
+
+(def debug? ^boolean goog.DEBUG)
+
+(defn make-group-logger [group-fn]
+      (fn [& args]
+          (when debug?
+                (if (oget+ js/console group-fn)
+                  (oapply+ js/console group-fn (vec args))
+                  (oapply+ js/console :log (concat ["==>"] (vec args)))))))
+
+(defn group-end []
+      (when debug?
+            (if (oget js/console :?groupEnd)
+              (ocall js/console :groupEnd)
+              (ocall js/console :log "<=="))))
+
+(defn make-level-loger [level]
+      (if debug?
+        (let [level-fn (if (oget+ js/console level) level :log)]
+             (fn [& args]
+                 (oapply+ js/console level (vec args))))
+        (constantly nil)))
+
+(def group (make-group-logger :?group))
+(def group-collapsed (make-group-logger :?groupCollapsed))
+(def log (make-level-loger :?log))
+(def warn (make-level-loger :?warn))
+(def error (make-level-loger :?error))
+(def info (make-level-loger :?info))
+
+(defn pp [& args]
+      (when debug?
+            (apply log (map (fn [a] (with-out-str (cljs.pprint/pprint a))) args))))
