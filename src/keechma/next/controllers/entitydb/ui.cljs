0532ef5bfa93de24b4c02f86927521544b9eb404
Mihael Konjević <konjevic@gmail.com>
2021-05-14 22:55:49 +0200

    Don't use pr-str in entitydb hooks
    pr-str tends to be slow, use memoization and clojure equality to compare ident and include-query

--- src/keechma/next/controllers/entitydb/ui.cljs
+++ src/keechma/next/controllers/entitydb/ui.cljs
@@ -3,22 +3,33 @@
             [keechma.next.helix.core :refer [use-sub]]
             [keechma.entitydb.core :as edb]))
 
+(defn use-memoized-deps [& deps]
+  (let [deps' (into [] deps)
+        ref* (hooks/use-ref deps')]
+    (when (not= @ref* deps')
+      (reset! ref* deps'))
+    @ref*))
+
 (defn use-entity-from-ident
   ([props edb-key ident]
    (use-entity-from-ident props edb-key ident nil))
   ([props edb-key ident include-query]
-   (let [getter (hooks/use-callback
-                 [(pr-str ident) (pr-str include-query)]
+   (let [deps (use-memoized-deps ident include-query)
+         getter (hooks/use-callback
+                 deps
                  (fn [entitydb]
-                   (edb/get-entity-from-ident entitydb ident include-query)))]
+                   (when ident
+                     (edb/get-entity-from-ident entitydb ident include-query))))]
      (use-sub props edb-key getter))))
 
 (defn use-entities-from-idents
-  ([props edb-key ident]
-   (use-entities-from-idents props edb-key ident nil))
-  ([props edb-key ident include-query]
-   (let [getter (hooks/use-callback
-                 [(pr-str ident) (pr-str include-query)]
+  ([props edb-key idents]
+   (use-entities-from-idents props edb-key idents nil))
+  ([props edb-key idents include-query]
+   (let [deps (use-memoized-deps idents include-query)
+         getter (hooks/use-callback
+                 deps
                  (fn [entitydb]
-                   (edb/get-entities-from-idents entitydb ident include-query)))]
+                   (when (seq idents)
+                     (edb/get-entities-from-idents entitydb idents include-query))))]
      (use-sub props edb-key getter))))
